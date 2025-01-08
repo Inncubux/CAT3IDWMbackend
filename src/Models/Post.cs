049e075554e60ee9b838900ccf1c0a5f6ffab0ea
Axel <axel.mondaca@alumnos.ucn.cl>
2025-01-08 18:20:21 -0300

    fix: added validation attribute to models

--- src/Models/Post.cs
+++ src/Models/Post.cs
@@ -8,9 +8,9 @@ namespace API.src.Models
 {
     public class Post
     {
-        public string Title { get; set; }
+        public string Title { get; set; } = string.Empty;
         public DateTime PublicationDate { get; set; }
-        public string ImageUrl {get; set;}
-        public User User {get; set;}
+        public string ImageUrl {get; set;} = string.Empty;
+        public User User {get; set;} = null!;
     }
 }
--- src/Models/User.cs
+++ src/Models/User.cs
@@ -4,6 +4,7 @@ using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Identity;
+using API.src.ValidationAttributes;
 
 
 namespace API.src.Models
@@ -11,8 +12,8 @@ namespace API.src.Models
     public class User: IdentityUser
     {
         [EmailAddress]
-        public string Email {get; set;}
-        [StringLength(100, MinimumLength = 6)]
-        public string Password { get; set; }
+        public string Email {get; set;} = string.Empty!;
+        [PasswordAttribute(6)]
+        public string Password { get; set; } = string.Empty!;
     }
 }
