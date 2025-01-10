a102beab5587d5fd80a166cb4fe3292b99e1222c
Axel <axel.mondaca@alumnos.ucn.cl>
2025-01-10 01:02:18 -0300

    feat: create post and user DTO

---
+++ src/DTOs/CreatePostDto.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.src.DTOs
+{
+    public class CreatePostDto
+    {
+        
+    }
+}
