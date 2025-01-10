d1ee7f6d40666ea4611f4f9ceac56ce6623e0aeb
Axel <axel.mondaca@alumnos.ucn.cl>
2025-01-10 02:04:33 -0300

    fix: added attributes and validations

--- src/DTOs/Auth/CreateUserDto.cs
+++ src/DTOs/Auth/CreateUserDto.cs
@@ -1,12 +1,17 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
+using API.src.ValidationAttributes;
 
 namespace API.src.DTOs.Auth
 {
     public class CreateUserDto
     {
-        
+        [EmailAddress]
+        public required string Email { get; set; } = string.Empty!;
+        [PasswordAttribute(6)]
+        public required string Password { get; set; } = string.Empty!;
     }
 }
